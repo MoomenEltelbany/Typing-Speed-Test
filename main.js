4eb4fabb20ef49a75de0e76987a1e6f08ba740b1
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2022-10-01 15:12:18 -0400

    Typing speed project
    Added new features of showing the score of each level

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ let scoreGot = document.querySelector(".got");
 let wholeLengthOfWords = document.querySelector(".total");
 let finishDiv = document.querySelector(".finish");
 let restartBtn = document.querySelector(".restart");
+let spanEasyWon = document.querySelector(".score .won");
+let spanEasyLost = document.querySelector(".score .lost");
+let spanMediumWon = document.querySelector(".medium .won");
+let spanMediumLost = document.querySelector(".medium .lost");
+let spanDifficultWon = document.querySelector(".difficult .won");
+let spanDifficultLost = document.querySelector(".difficult .lost");
 
 // Array of Words
 const easyWords = [
@@ -48,7 +54,6 @@ const mediumWords = [
     "Coding",
     "Function",
 ];
-
 const difficultWords = [
     "Programming",
     "Destructuring",
@@ -99,7 +104,6 @@ startBtn.onclick = () => {
     // A check to see which array we will work with and this is based on the difficulty level that the user will choose
     if (defaultLevel.innerHTML === "Easy") {
         wholeLengthOfWords.innerHTML = easyWords.length;
-
         generateWords(easyWords);
     } else if (defaultLevel.innerHTML === "Medium") {
         wholeLengthOfWords.innerHTML = mediumWords.length;
@@ -110,11 +114,60 @@ startBtn.onclick = () => {
     }
 };
 
+// localStorage.clear();
+// Making the counter of each difficulty both for won and lost as zero, default
+let counterEasyWon = 0;
+let counterEasyLost = 0;
+let counterMediumWon = 0;
+let counterMediumLost = 0;
+let counterDifficultWon = 0;
+let counterDifficultLost = 0;
+
+// In the upcoming conditions is, if there is a value inside the local storage, it means that it is not the first time the user
+// play, so we set the counter of each difficulty level to the level of the local storage
+if (window.localStorage.getItem("counterEasyWon")) {
+    counterEasyWon = window.localStorage.getItem("counterEasyWon");
+}
+if (window.localStorage.getItem("counterEasyLost")) {
+    counterEasyLost = window.localStorage.getItem("counterEasyLost");
+}
+if (window.localStorage.getItem("counterMediumWon")) {
+    counterMediumWon = window.localStorage.getItem("counterMediumWon");
+}
+if (window.localStorage.getItem("counterMediumLost")) {
+    counterMediumLost = window.localStorage.getItem("counterMediumLost");
+}
+if (window.localStorage.getItem("counterDifficultWon")) {
+    counterDifficultWon = window.localStorage.getItem("counterDifficultWon");
+}
+if (window.localStorage.getItem("counterDifficultLost")) {
+    counterDifficultLost = window.localStorage.getItem("counterDifficultLost");
+}
+
+// Here we set the counter of each to the right place in the storage
+window.localStorage.setItem("counterEasyWon", counterEasyWon);
+window.localStorage.setItem("counterEasyLost", counterEasyLost);
+window.localStorage.setItem("counterMediumWon", counterMediumWon);
+window.localStorage.setItem("counterMediumLost", counterMediumLost);
+window.localStorage.setItem("counterDifficultWon", counterDifficultWon);
+window.localStorage.setItem("counterDifficultLost", counterDifficultLost);
+
+// Here we append the storage inside the designed div
+spanEasyWon.innerHTML = window.localStorage.getItem("counterEasyWon");
+spanEasyLost.innerHTML = window.localStorage.getItem("counterEasyLost");
+spanMediumWon.innerHTML = window.localStorage.getItem("counterMediumWon");
+spanMediumLost.innerHTML = window.localStorage.getItem("counterMediumLost");
+spanDifficultWon.innerHTML = window.localStorage.getItem("counterDifficultWon");
+spanDifficultLost.innerHTML = window.localStorage.getItem(
+    "counterDifficultLost"
+);
+
 // A function to generate random words
 let generateWords = (array) => {
     // To remove the inner text inside the div where we will show the array of words
     upcomingWords.innerHTML = "";
 
+    // If the array still have elements, it means that the user still have words to write
     if (array.length > 0) {
         // Show a random word for the user to type
         let randomWordValue = array[Math.floor(Math.random() * array.length)];
@@ -129,7 +182,30 @@ let generateWords = (array) => {
             div.appendChild(document.createTextNode(array[i]));
             upcomingWords.appendChild(div);
         }
+        countTime(defaultSeconds.innerHTML);
     } else {
+        if (defaultSeconds.innerHTML === "6") {
+            counterEasyWon++;
+            window.localStorage.setItem("counterEasyWon", counterEasyWon);
+            spanEasyWon.innerHTML =
+                window.localStorage.getItem("counterEasyWon");
+        } else if (defaultSeconds.innerHTML === "4") {
+            counterMediumWon++;
+            window.localStorage.setItem("counterMediumWon", counterMediumWon);
+            spanMediumWon.innerHTML =
+                window.localStorage.getItem("counterMediumWon");
+        } else {
+            counterDifficultWon++;
+            window.localStorage.setItem(
+                "counterDifficultWon",
+                counterDifficultWon
+            );
+            spanDifficultWon.innerHTML = window.localStorage.getItem(
+                "counterDifficultWon"
+            );
+        }
+
+        // Ih the user could write all the words correctly
         const congratDiv = document.createElement("div");
         congratDiv.className = "good";
         congratDiv.appendChild(
@@ -140,7 +216,6 @@ let generateWords = (array) => {
         finishDiv.prepend(congratDiv);
         restartBtn.style.display = "block";
     }
-    countTime(defaultSeconds.innerHTML);
 };
 
 // Function to start counting the time based on the difficulty level, that is why I added the level as a parameter because based on it will appear the counting
@@ -163,6 +238,32 @@ let countTime = () => {
                 // This we use to generate words from the array that the user chose in the beginning of the game
                 decideDiff(defaultLevel.innerHTML);
             } else {
+                if (defaultSeconds.innerHTML === "6") {
+                    counterEasyLost++;
+                    window.localStorage.setItem(
+                        "counterEasyLost",
+                        counterEasyLost
+                    );
+                    spanEasyLost.innerHTML =
+                        window.localStorage.getItem("counterEasyLost");
+                } else if (defaultSeconds.innerHTML === "4") {
+                    counterMediumLost++;
+                    window.localStorage.setItem(
+                        "counterMediumLost",
+                        counterMediumLost
+                    );
+                    spanMediumLost.innerHTML =
+                        window.localStorage.getItem("counterMediumLost");
+                } else {
+                    counterDifficultLost++;
+                    window.localStorage.setItem(
+                        "counterDifficultLost",
+                        counterDifficultLost
+                    );
+                    spanDifficultLost.innerHTML = window.localStorage.getItem(
+                        "counterDifficultLost"
+                    );
+                }
                 restart();
                 restartBtn.style.display = "block";
             }
